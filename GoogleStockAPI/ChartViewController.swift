c416ee70268d1e42dd4d31674f5a1890cdd8d445
Warren Hansen <whansen1@mac.com>
2017-09-09 09:47:54 -0700

    task: linking wrapper file

--- GoogleStockAPI/ChartViewController.swift
+++ GoogleStockAPI/ChartViewController.swift
@@ -25,6 +25,12 @@ class ChartViewController: UIViewController {
     
     var sciChartSurface: SCIChartSurface?
     
+    var lineDataSeries: SCIXyDataSeries!
+    var scatterDataSeries: SCIXyDataSeries!
+    
+    var lineRenderableSeries: SCIFastLineRenderableSeries!
+    var scatterRenderableSeries: SCIXyScatterRenderableSeries!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -37,25 +43,66 @@ class ChartViewController: UIViewController {
                 for thing in self.dataFeed.priceHistory {
                     print(thing.ticker! + " " + thing.date! +  " \(thing.close!)")
                 }
-                //self.updateGraphCharts()
                 self.setUpChartUI()
             }
         }
+        
+        sciChartSurface = SCIChartSurface(frame: self.view.bounds)
+        sciChartSurface?.translatesAutoresizingMaskIntoConstraints = true
+        
+        self.view.addSubview(sciChartSurface!)
+        
+        sciChartSurface?.xAxes.add(SCINumericAxis())
+        sciChartSurface?.yAxes.add(SCINumericAxis())
+        
+        createDataSeries()
+        createRenderableSeries()
     }
     
     func setUpChartUI() {
         
-        sciChartSurface = SCIChartSurface(frame: self.view.bounds)
-        sciChartSurface?.translatesAutoresizingMaskIntoConstraints = true
-        self.view.addSubview(sciChartSurface!)
-        
-        // create axis
-        sciChartSurface?.xAxes.add(SCINumericAxis())
-        sciChartSurface?.yAxes.add(SCINumericAxis())
-        
     }
+    
+    func createDataSeries(){
+        // Init line data series
+        lineDataSeries = SCIXyDataSeries(xType: .double, yType: .double)
+        for i in 0..<500{
+            lineDataSeries.appendX(SCIGeneric(i), y: SCIGeneric(sin(Double(i))*0.01))
+        }
+        
+        // Init scatter data series
+        scatterDataSeries = SCIXyDataSeries(xType: .double, yType: .double)
+        for i in 0..<500{
+            scatterDataSeries.appendX(SCIGeneric(i), y: SCIGeneric(cos(Double(i))*0.01))
+        }
+    }
+    
+    func createRenderableSeries(){
+        lineRenderableSeries = SCIFastLineRenderableSeries()
+        lineRenderableSeries.dataSeries = lineDataSeries
+        
+        scatterRenderableSeries = SCIXyScatterRenderableSeries()
+        scatterRenderableSeries.dataSeries = scatterDataSeries
+        
+        sciChartSurface?.renderableSeries.add(lineRenderableSeries)
+        sciChartSurface?.renderableSeries.add(scatterRenderableSeries)
+    }
+    
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
